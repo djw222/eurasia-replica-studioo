c29aaa274fc58b157d95877e2a777a127f8aa375
djw222 <darrellwells222@gmail.com>
2025-12-06 13:55:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eurasia-replica-studioo/', // <-- change this to your repo name, keep leading and trailing slashes
+  base: '/eurasia-replica-studioo/', // <-- THIS MUST MATCH YOUR REPO NAME
 })
