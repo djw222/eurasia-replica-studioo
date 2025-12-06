30707ab5df054428a8f893188ed45feca7600a41
djw222 <darrellwells222@gmail.com>
2025-12-06 16:05:04 +0100

    Enhance Vite config with path alias and imports
    Added path alias resolution to the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eurasia-replica-studioo/', // 
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/eurasia-replica-studioo/",
+});
